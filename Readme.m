8d366843ed43052bd33df267213383a806c2af23
Matt98x <matteo.palmas7gmail.com>
2020-10-22 09:08:25 +0000

    Start of working assumption
    first modification to the readme, to see if the layout works

--- Readme.m
+++ Readme.m
@@ -1,1 +1,11 @@
-# Experimental robotic assignment 1
+# Experimental robotic assignment1
+
+## Working assumptions
+
+- The robot, simulating a pet, interact with a human and moves in a discrete 2D environment.
+- Both the robot targets and its positions exclusively to this plane.
+- The robot receive forms in strings with possible form:
+	-"play"	
+	-"go to x1 y1"
+	-"point to x1 y1"
+	- combination of at maximum 2 commands
